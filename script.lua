df45a5aecb60384d16741fc26be5c5a9b8ee762e
chrisvm <christian.velez55@gmail.com>
2017-04-29 23:27:57 -0400

    changed test script

--- script.lua
+++ script.lua
@@ -4,18 +4,30 @@ function setup()
 	print('screen width: ' .. window.width)
 	print('screen height: ' .. window.height)
 
-	shape = CircleShape.new(50.0)
-	shape:setFillColor(color.White)
+	local shape1 = CircleShape.new(10.0)
+	shape1:setFillColor(color.White)
+
+	local shape2 = CircleShape.new(30.0)
+	shape2:setFillColor(color.Red)
+
+	local shape3 = CircleShape.new(50.0)
+	shape3:setFillColor(color.Blue)
+
+	shapes = { shape1, shape2, shape3 }
 end
 
 function update()
 	local delta = 0.001
-	shape:move(delta, delta)
+	for index = 1, #shapes do
+		shapes[index]:move(delta, delta)
+	end
 end
 
 function draw()
 	window.clear()
 	update()
-	shape:draw()
+	for index = 1, #shapes do
+		shapes[index]:draw()
+	end
 	window.display()
 end
